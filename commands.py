c22d3f4b99d46fe633fef844e1b57f919dba4aa1
PetraNemcekova <101061242+PetraNemcekova@users.noreply.github.com>
2024-09-27 11:14:51 +0200

    Update commands.py

--- commands.py
+++ commands.py
@@ -7,20 +7,36 @@ plt.imshow(image1)
 plt.title('Nadpis prvniho podobrazku')
 plt.xlabel('Popis osi x [jednotky]')
 plt.ylabel('Popis osi y [jednotky]')
+
 plt.subplot(1,2,2)
 plt.hist(image1.ravel())
 plt.title('Nadpis druheho podobrazku')
 plt.xlabel('Popis osi x [jednotky]')
 plt.ylabel('Popis osi y [jednotky]')
 plt.show()
+
 plt.close('all')
 
 # %% NUMPY
 
 import numpy as np
 
+image = np.zeros_like(image)
+image = np.ones(x_size, y_size)
 img_shape = np.shape(image)
+vector = np.linspace(start = start, stop = stop, num = num)
+
+spectrum = amplitude_spectrum * np.exp(1j*phase_spectrum)
+spectrum_1 = np.log(np.fft.fftshift(np.abs(np.fft.fft2(image))))
+spectrum_2 = np.fft.fftshift(np.angle(np.fft.fft2(image)))
+image = np.real(np.fft.ifft2(spectrum))
+
+# %% SCIPY
+import scipy
+image = scipy.signal.convolve2d(image, impulse_response, mode = 'mode_type')
 
 # %% SKIMAGE
-import skimage.io
-img = skimage.io.imread('path_to_image')
+import skimage
+image = skimage.io.imread('path_to_image')
+gs_image = skimage.color.rgb2gray(image)
+image = skimage.transform.rotate(image,angle)
